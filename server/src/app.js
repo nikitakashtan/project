0ec4079a379d67df4e56f1e58dd6da77f84e91a8
nikitakashtan <nikita-ka@yandex.ru>
2024-05-01 12:46:45 +0300

    starting

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+// const cookieParser = require('cookie-parser');
+// const authRouter = require('./router/authRouter');
+// const tokensRouter = require('./router/tokensRouter');
+// const postsRouter = require('./router/postsRouter');
+
+const app = express();
+
+app.use(morgan('dev'));
+// app.use(cookieParser());
+app.use(express.json());
+
+// app.use('/api/tokens', tokensRouter);
+// app.use('/api/auth', authRouter);
+// app.use('/api/posts', postsRouter);
+
+module.exports = app;
