12e2fdce0317db8438ce10dd95ecc1be18d3aaac
nikitakashtan <nikita-ka@yandex.ru>
2024-05-01 18:00:27 +0300

    onerouter

---
+++ server/src/routes/candidatesRouter.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Canditate, Stage } = require('../../db/models');
+const verifyAccessToken = require('../middlewares/verifyAccessToken');
+
+const candidatesRouter = express.Router();
+
+candidatesRouter.route('/')
+  .get(async (req, res) => {
+    const candidates = await Canditate.findAll({
+      include: {
+        model: Stage,
+      },
+    });
+    res.json(candidates);
+  })
+  .post(verifyAccessToken, async (req, res) => {
+    const newPost = await Canditate.create({ ...req.body, userId: res.locals.user.id });
+    const candidate = await Canditate.findByPk(newPost.id, { include: [Stage] });
+    res.json(candidate);
+  });
+
+module.exports = candidatesRouter;
